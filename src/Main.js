a9a07688578af410c0712cf83707bd7f6a7cc494
Samuelote <samote.wood@gmail.com>
2018-12-26 20:32:22 -0700

    added react-dnd.

--- src/Main.js
+++ src/Main.js
@@ -3,15 +3,34 @@ import { Provider } from 'react-redux';
 import {store} from './reducers'
 import TaskList from './components/taskList';
 import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 class App extends Component {
+  onBeforeDragStart = () => {
+    /*...*/
+  };
+
+  onDragStart = () => {
+    /*...*/
+  };
+  onDragUpdate = () => {
+    /*...*/
+  };
+  onDragEnd = () => {
+    // the only one that is required
+    console.log("hello")
+  };
   render() {
     return (
       <Provider store={store}>
-        <div> 
+        <DragDropContext
+          onBeforeDragStart={this.onBeforeDragStart}
+          onDragStart={this.onDragStart}
+          onDragUpdate={this.onDragUpdate}
+          onDragEnd={this.onDragEnd}
+        >
           <TaskList />
-          
-        </div>
+        </DragDropContext>
       </Provider>
     );
   }
--- src/components/taskList/view.js
+++ src/components/taskList/view.js
@@ -3,9 +3,9 @@ import Style from './style.css';
 import CheckBoxStyle from './CheckBoxStyle.css';
 import Input from '../taskInput';
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import { Droppable } from 'react-beautiful-dnd';
 
 export default class List extends Component {
-
     constructor(){
       super();
       this.state = {
@@ -15,63 +15,94 @@ export default class List extends Component {
 
     //This disables buttons by checking if any inputs are checked
     ifChecked(e){
-      const ul = e.parentElement.parentElement.children;
-      let bool;
-
-      //checkbox always has to be first element in <li>
-      for (let i = 0; i < ul.length; i++){
-        const checked = ul[i].children[0].children[0].checked;
-
-        if (checked){
-          bool = true;
-          break;
-        } else bool = false;
-      }
-      this.setState({moveDisable: bool});
+      
     }
 
     checkItem(e) {
-      // console.log(e.target.parentElement)
+      const el = e.target.parentElement.parentElement.parentElement;
       e.target.parentElement.children[0].checked = !e.target.parentElement.children[0].checked;
-      this.ifChecked(e.target.parentElement);
+      console.log(this);
+      let bool;
+      
+      // //checkbox always has to be first element in <li>
+      // for (let i = 0; i < el.children.length; i++){
+      //   const checked = el.children[i].children[0].children[0].checked;
+      //   if (checked){
+      //     bool = true;
+      //     break;
+      //   } else bool = false;
+      // }
+      // this.setState({moveDisable: bool});
     }
+
     render() {
       const {tasks, down, up} = this.props;
-      const {moveDisable} = this.state;
       return (
         <div className="MainContainer">
-        {
-          tasks.map((text,i)=>
-          {
-            return (
-              <div className="Row" key={i}>
-                <div className="checkContainer">
-                  <input className="CheckBox" type="checkbox" />
-                  <span className="checkmark" onClick={(e)=>this.checkItem(e)}></span>
-                </div>
-                <input placeholder={text} type="text" />
-                <div className="ArrowContainer">
-                  <button disabled={ moveDisable } onClick={(e)=>
-                  {
-                    up(i); 
-                    e.stopPropagation()
-                    }
-                  }><FaAngleUp /></button>
-
-                  <button disabled={ moveDisable } onClick={(e)=>
-                  {
-                    down(i) 
-                    e.stopPropagation()
-                    }
-                  }><FaAngleDown /></button>
-                </div>
-              </div>
-            )
-          })
-        }
-        <div className="Row"><Input /></div>
+         <Droppable droppableId="list">
+          {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={{
+                backgroundColor: provided.isDragging ? 'green' : 'lightblue',
+              }}
+              {...provided.droppableProps}
+            >
+              {
+                tasks.map((text,i)=>
+                {
+                  return (
+                  <Task 
+                    checkItem={this.checkItem} 
+                    key={i} 
+                    text={text} 
+                    i={i} 
+                    props={this.props} 
+                    moveDisable={this.state.moveDisable}
+                  />
+                  );
+                })
+              }
+              <div className="Row"><Input /></div>
+            </div>
+          )}
+        </Droppable>
         </div>
       );
+    }
+  }
+
+
+  class Task extends Component {
+
+    render() {
+      const {down, up} = this.props.props;
+      const {i, checkItem, moveDisable} = this.props;
+
+      return (
+        <div className="Row">
+          <div className="checkContainer">
+            <input className="CheckBox" type="checkbox" />
+            <span className="checkmark" onClick={(e)=>checkItem(e)}></span>
+          </div>
+          <input placeholder={this.props.text} type="text" />
+          <div className="ArrowContainer">
+            <button disabled={ moveDisable } onClick={(e)=>
+            {
+              up(i); 
+              e.stopPropagation()
+              }
+            }><FaAngleUp /></button>
+
+            <button disabled={ moveDisable } onClick={(e)=>
+            {
+              down(i) 
+              e.stopPropagation()
+              }
+            }><FaAngleDown /></button>
+          </div>
+        </div>
+      )
     }
   }
   
