acda98d44c78a114e338d67811cefc1891b49025
Samuelote <samote.wood@gmail.com>
2019-08-06 11:36:09 -0600

    Added item dragging to cypress tests

--- cypress/integration/add_todos.spec.js
+++ cypress/integration/add_todos.spec.js
@@ -1,4 +1,5 @@
 import Input from "../../src/components/taskInput/view";
+import * as keyCodes from "./keycodes.js";
 
 describe("My First Test", function() {
   it("adds task", function() {
@@ -9,21 +10,44 @@ describe("My First Test", function() {
       .type("Do Laundry{enter}")
       .type("Go to work{enter}")
       .type("Take kids to soccer game{enter}")
-      .type("Pubg{enter}")
-      .type("Eat dinner{enter}")
-      .type("Clean the house");
+      .type("Pubg{enter}");
 
     cy.get(".AddTaskContainer").click();
   });
 
   it("checks off tasks", () => {
     cy.get(".MainContainer > div").each((e, i) => {
-      if (e[0].className === "Row" && i > 2) {
+      if (e[0].className === "Row" && i % 2 !== 0) {
         cy.get(".MainContainer > div")
           .eq(i)
           .find(".checkContainer")
           .find(".checkmark")
           .click({ force: true });
+      }
+    });
+  });
+
+  it("Drag and drop checked tasks", () => {
+    cy.get(".MainContainer > div").each((e, i) => {
+      if (i < 4) {
+        cy.get("[data-react-beautiful-dnd-drag-handle]")
+          .eq(i)
+          .as(`${i}`);
+      }
+    });
+
+    cy.get(".MainContainer > div").each((e, i) => {
+      var upOrDown = Math.random() >= 0.5;
+      if (i < 4) {
+        cy.get(`@${i}`)
+          .focus()
+          .trigger("keydown", { keyCode: keyCodes.space })
+          .trigger("keydown", {
+            keyCode: upOrDown ? keyCodes.arrowDown : keyCodes.arrowUp,
+            force: true
+          })
+          .wait(1000)
+          .trigger("keydown", { keyCode: keyCodes.space, force: true });
       }
     });
   });
